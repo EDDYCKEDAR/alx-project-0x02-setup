a99eb78e51d1611fd63a99577a5aa3fc9dbf711f
Eddy Ckedar <eddykedar@gmail.com>
2025-07-25 18:23:18 +0200

    Create index.ts

---
+++ alx-project-0x02/interfaces/index.ts
@@ -0,0 +1,55 @@
+// Card Component Props
+export interface CardProps {
+  title: string;
+  content: string;
+}
+
+// Button Component Props
+export interface ButtonProps {
+  children: React.ReactNode;
+  size: 'small' | 'medium' | 'large';
+  shape: 'rounded-sm' | 'rounded-md' | 'rounded-full';
+  onClick?: () => void;
+  type?: 'button' | 'submit' | 'reset';
+  className?: string;
+}
+
+// Post Props
+export interface PostProps {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+// User Props
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+// Modal Props
+export interface PostModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (data: { title: string; content: string }) => void;
+}
